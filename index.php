a38502999f8c0488a191598377a2323d134abf0a
Maximusse <dekomaximusse@gmail.com>
2024-09-06 09:49:09 +0000

    Initialisation

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentification | EMGA</title>
+    <link rel="stylesheet" href="style/bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="page-title">
+            <h1>EMGA APP</h1>
+        </div>
+        <div class="login-bloc row">
+            <div class="col-lg-12">
+                <div class="login-content">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="style/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="style/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
